6ee356d7a4089e29e8e76dab90a70983624e08dd
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-19 13:59:11 -0400

    updating class names

--- scripts/model.js
+++ scripts/model.js
@@ -1,16 +1,55 @@
-(function(window) {
+(function(window){
 
-  var game = [ //list of 5-tuples representing fromRank, fromFile, toRank, toFile. 
-         {
-             fromRank: '.rank-2',
-             fromFile: '.file-d',
-             toRank: '.rank-4',
-             toFile: '.file-d',
-             piece: 'whitepawn',
-         }
+  var chessGame = [ //List of 5 tuples representing from.rank, fromFile, to.rank, toFile.
+        // Two dimensional array:
+    ['.row rank-2','.square file-d','.row rank-4','square file-d','whitepawn'],
+    ['.row rank-8','.square file-g','.row rank-6','square file-f','blacknight'],
+    ['.row rank-2','.square file-c','.row rank-4','square file-c','whitepawn'],
+    ['.row rank-7','.square file-e','.row rank-6','square file-e','blackpawn'],
+    ['.row rank-2','.square file-g','.row rank-3','square file-g','whitebishop'],
+    ['.row rank-7','.square file-d','.row rank-5','square file-d','blackbishop'],
+    ['.row rank-1','.square file-f','.row rank-2','square file-g','whiteknight'],
+    ['.row rank-8','.square file-f','.row rank-7','square file-e','blackking'],
+    ['.row rank-1','.square file-g','.row rank-3','square file-f','blackrook'],
+    ['.row rank-8','.square file-e','.row rank-8','square file-g','whiteking'],
+    ['.row rank-8','.square file-h','.row rank-8','square file-f','whiterook'],
+    ['.row rank-1','.square file-e','.row rank-1','square file-g','blackpawn'],
+    ['.row rank-1','.square file-h','.row rank-1','square file-f','whiterook'],
+    ['.row rank-5','.square file-d','.row rank-4','square file-c','blackpawn']
+  ];
 
-     ];
+  var reverseGame = [
 
+    ['.row rank-4','.square file-e','.row rank-8','square file-g','whiteking'],
+    ['.row rank-6','.square file-h','.row rank-8','square file-f','whiterook'],
+    ['.row rank-4','.square file-e','.row rank-1','square file-g','blackpawn'],
+    ['.row rank-6','.square file-h','.row rank-1','square file-f','whiterook'],
+    ['.row rank-3','.square file-d','.row rank-4','square file-c','blackpawn'],
+    ['.row rank-5','.square file-e','.row rank-8','square file-g','whiteking'],
+    ['.row rank-2','.square file-h','.row rank-8','square file-f','whiterook'],
+    ['.row rank-7','.square file-e','.row rank-1','square file-g','blackpawn'],
+    ['.row rank-3','.square file-h','.row rank-1','square file-f','whiterook']
+  ];
+
+  var counter = 0;
+
+  function move( whichMove ){
+    $( whichMove[0] + ' > ' + whichMove[1] ).removeClass( whichMove[4] );
+    $( whichMove[2] + ' > ' + whichMove[3] ).addClass( whichMove[4] );
+  }
+
+  function movePrev( whichMove ){
+    $(whichMove[2] + '>' + whichMove[3]).removeClass(whichMove[4]);
+    $(whichMove[0] + '>' + whichMove[1]).addClass(whichMove[4]);
+  };
+
+  window.chess = {
+    "game": game,
+    "reverseGame": reverseGame,
+    "move": move,
+    "counter": counter,
+    "movePrev": movePrev,
+  };
 })(window);
 
 
@@ -41,14 +80,14 @@
 // From MDN (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Creating_a_two-dimensional_array):
 
 // var board = [
-//   ['R','N','B','Q','K','B','N','R'],
-//   ['P','P','P','P','P','P','P','P'],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   [' ',' ',' ',' ',' ',' ',' ',' '],
-//   ['p','p','p','p','p','p','p','p'],
-//   ['r','n','b','q','k','b','n','r'] ];
+  // ['R','N','B','Q','K','B','N','R'],
+  // ['P','P','P','P','P','P','P','P'],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // [' ',' ',' ',' ',' ',' ',' ',' '],
+  // ['p','p','p','p','p','p','p','p'],
+  // ['r','n','b','q','k','b','n','r'] ];
 //
 // console.log(board.join('\n') + '\n\n');
 //
