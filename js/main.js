19493f6794099f196e35344212beeb5c0c86fc45
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-19 13:58:35 -0400

    clean up clean up...

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 (function(window) {
 
-$('.play').click(function() {
+  $('.last').click(function() {
+    for (chess.counter; chess.counter < chess.game.length; chess.counter++){
+      chess.move(chess.game[chess.counter]);
+    };
+  });
 
-  moveThePiece(2, 'd', 4, 'd', 'whitepawn');
-  moveThePiece(8, 'g', 6, 'f', 'blackknight');
-  moveThePiece(2, 'c', 4, 'c', 'whitepawn');
+  $('.next').click(function(){
+    if (chess.counter < chess.game.length){
+      chess.move(chess.game[chess.counter]);
+      chess.counter = chess.counter + 1;
+    };
+  });
 
-});
+  $('.previous').click(function(){
+    if (chess.counter < chess.game.length){
+      chess.movePrev(chess.game[chess.counter]);
+      chess.counter = chess.counter - 1;
+    };
+  });
+})(window);
 
-/**
+
+/*
      * What does moveThePiece do?
-     *
      *
      * @param {Number} fromRank
      * @param {String} fromFile
@@ -19,57 +32,10 @@ $('.play').click(function() {
      * @param {String} piece
      * @return undefined
      */
-    function moveThePiece(fromRank, fromFile, toRank, toFile, piece) {
-
-        $('.rank-' + fromRank + ' > .file-' + fromFile).removeClass(piece);
-        $('.rank-' + toRank + ' > .file-' + toFile).addClass(piece);
-    }
-
-  }
-)(window);
-
-
-
-
-
-
-
-
-
-
-
-
-// From Jorge's Chessboard (Monday's Lecture):
-//     $('.play').click(function() {
-//
-//       // $('.rank-2 > .file-d').removeClass('whitepawn');
-//       // $('.rank-4 > .file-d').removeClass('whitepawn');
-//       moveThePiece(2, 'd', 4, 'd', 'whitepawn');
-//
-//       // $('.rank-8 > .file-g').removeClass('blacknight');
-//       // $('.rank-6 > .file-f').removeClass('blacknight');
-//       moveThePiece(8, 'g', 6, 'f', 'whitepawn');
-//
-//       moveThePiece(2, 'c', 4, 'c', 'whitepawn');
-//
-//     });
-//
-// /**
-// * What does moveThePiece do?
-// *
-// *
-// *
-// * @param {Number} fromRank
-// * @param {String} fromFile
-// * @param {Number} toRank
-// * @param {String} toFile
-// * @return undefined
-// **/
-// // The following function is to move the piece:
-// function moveThePiece(fromRank, fromFile, toRank, toFile, piece) {
-//
-// };
-//
-// $(fromFile + '>' + 'rank-2').removeClass(piece);
-// $(toRank + '>' + toFile).addClass(piece);
-//
+  //   function moveThePiece(fromRank, fromFile, toRank, toFile, piece) {
+  //
+  //       $('.rank-' + fromRank + ' > .file-' + fromFile).removeClass(piece);
+  //       $('.rank-' + toRank + ' > .file-' + toFile).addClass(piece);
+  //   }
+  //
+  // }
